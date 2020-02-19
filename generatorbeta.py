e1192ed22547a07458e2884d35216c90e2b3fcba
eplank18 <eplank2@vols.utk.edu>
2020-02-19 18:39:55 -0500

    Added constant updating.

--- generatorbeta.py
+++ generatorbeta.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
 import sys
+import time
 
 class garage:
     def __init__(self, ver, size=0, floor_count=0, size_per_floor=0):
         self.ver = ver
         self.per_fill = np.random.randint(0, 100, 1)
         self.size = size
+	self.size_per_floor = size_per_floor 
         
         if(ver == 0):
             filler = []
@@ -24,19 +26,22 @@ class garage:
             for i in range(0, floor_count):
                 filler.append(np.random.randint(0, size_per_floor, 1))
             self.model = pd.DataFrame(filler)
-    def update(self, ver):
-        if(ver == 0):
-
-        elif(ver == 1):
-        elif(ver == 2):
-        elif(ver == 3):
 
 
-
-
+if(len(sys.argv) != 3):
+	print("Usage: <modeltype> <sleep duration in seconds>")
+	exit(0)
 mtype = sys.argv[1] 
-d = {'one': np.random.randint(0, 2, 5), 'two': np.random.randint(0, 2, 5)}	
-df = pd.DataFrame(d, index = ['a', 'b', 'c', 'd', 'e'])
+frequency = float(sys.argv[2])
+#d = {'one': np.random.randint(0, 2, 5), 'two': np.random.randint(0, 2, 5)}	
+#df = pd.DataFrame(d, index = ['a', 'b', 'c', 'd', 'e'])
 a = garage(0, size_per_floor=40, floor_count=4)
 print(a.model)
-print (df.to_json(orient = 'split'))
+#print (df.to_json(orient = 'split'))
+while 1:
+	time.sleep(frequency)
+	for i in range(0, a.size_per_floor):
+		temp = a.model[i]
+		for j in range(0, temp.size):
+			temp[j] = np.random.randint(0, 2)
+	print(a.model)
